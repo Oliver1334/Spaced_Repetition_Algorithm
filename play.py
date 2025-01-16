92112c8065997d40338bccc33e14308e2f8b9956
Oliver1334 <oliver.taylor95@outlook.com>
2025-01-16 23:33:11 +0000

    initialise play.py file

---
+++ play.py
@@ -0,0 +1,5 @@
+import csv
+import json
+
+#2 Create a game, where a user has to get through as many items in the list (i.e answer them correctly), and when they ultimately fail, then they get a high score and have to start again (forcing them to continue practicing)
+# 2.1 Create the files to read both the long list of space-repeated words so that we can loop over them and test the user continuously until they fail, at which point give them a score, and also the word definitions so that we may show a user a word definition if it's their first time encountering the word
