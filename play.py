95d7a8c8160a730da95168954c8aa92911eb4c79
Oliver1334 <oliver.taylor95@outlook.com>
2025-01-20 01:02:32 +0000

    test_user function, main game loop

--- play.py
+++ play.py
@@ -5,7 +5,6 @@ import json
 
 # 2.1 Create the files to read both the long list of space-repeated words from the CSV file and the word definitions from the definitions.json file. We can loop over the CSV list to test the user continuously until they fail, at which point give them a score. The json file will be used to give the user a word definition if it's their first time encountering the word. (also check their answer against correct definition)
 
-
 def load_words_from_csv(file_path):
     #loads a list of words from a csv file
     with open(file_path, 'r') as f:
@@ -22,3 +21,43 @@ def load_definitions_from_json(file_path):
         definitions = json.load(f)
     return definitions
 
+# 2.2 Define the main game loop, testing the user on each word in the CSV list one after another in the spaced repetition algorithm order. This while loop will exit only when each word has been mastered or the user makes a mistake.
+
+def test_user(words, definitions):
+    # Test the user on the definitions of the words provided.
+
+    high_score = 0
+    encountered_words = set() # Track which words the user has already encountered, set removes duplicates in array.
+
+    for word in words:
+        print(f'\nDefine: {word}')
+
+        # If we have a word that isn't defined, skip it - catches any errors in definitions JSON
+        if word not in definitions:
+            print('\nDefinition not found for this word. Skipping...')
+
+        # Confirmation word has definition, so we can test the user
+        # Returns value on key of 'word' for definition
+        definition = definitions[word]
+
+        # If it's the first time encountering the word, show the definition
+        if word not in encountered_words:
+            print(f'\n{word} -> \"{definition}\"')
+        
+        user_input = input('\nAnswer: ')
+
+        # Check if the user input matches the definition
+        if user_input.lower() != definition.lower():
+            print('\nGame Over!')
+            print(f'High Score: {high_score}')
+            print(f'Correct Definition: {definition}')
+            return
+        
+        # Add word to encountered words and increment the score as they answered correctly
+        encountered_words.add(word) #Adds to set
+        high_score += 1
+        print('Correct!')
+
+    # We have finished looping through all words in the CSV file, the user has won the game and mastered all definitions.
+    print('\nCongratulations! You have completed the game! (:)')
+    print(f'\nHigh Score: {high_score}')
